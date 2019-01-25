a5ca4ed1bf705f67f7196ff0e3248767c64f438d
ChelseaW <c.winfrey93@yahoo.com>
2019-01-25 16:02:43 -0600

    CW

---
+++ ObjectsCW.py
@@ -0,0 +1,75 @@
+
+# Create a class Dog. Make sure it has the attributes name, breed, color, gender.
+# Create a function that will print all attributes of the class.
+# Create an object of Dog in your problem1 function and print all of it's attributes.
+
+def main():
+    # problem1()
+    # problem2()
+    problem3()
+    # problem4()
+
+
+
+
+# class Dog:
+#     def __init__(self, name = "", breed = "", color = "", gender = ""):
+#         self.name = name
+#         self.breed = breed
+#         self.color = color
+#         self.gender = gender
+#
+#     def Dog2(self):
+#         print(self.name, self.breed, self.color, self.gender)
+#
+#
+#     # def Dog2(self):
+#     #     return("This is the dog" + self.color)
+
+
+person = Person
+    print(name = "Kate", weight = 132, height = 5'3)
+    print(name = "Dave", weight = 184, height = 6'1)
+    print(name = "Amber", weight = 108, height = 5'9)
+
+while(Person )
+
+
+
+
+
+
+
+
+
+
+# def problem1():
+#     myDogs = Dog("King ","German Shepard ", "Gray", "Female" )
+#     myDogs.Dog2()
+
+
+# def problem2():
+#
+#     work = ""
+#     while(work != '='):
+#         work = input("Something else")
+
+
+
+def problem3():
+
+#     In your main file create three Person objects.
+#     Change the weight and height of each one.
+#     Afterwards, print the BMI (body mass index) of each Person.
+#     If you don’t know how to calculate BMI, look at the code I provided for you.
+#
+# Hint: BMI is (weight / (height * height)) x 703. Weight is in pounds and height is in inches.
+#
+# Extra:Put the three person objects in an array and use a loop to print out their BMIs.
+#
+
+
+
+
+if __name__ == '__main__':
+    main()
